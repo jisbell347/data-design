a500364477cde32988aa1c94d9657c0375ece5cb
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-11 20:32:16 -0600

    Adjusted paths of anchor tags

--- epic/index.php
+++ epic/index.php
@@ -8,7 +8,7 @@
 	<body>
 		<nav class="nav nav-bar">
 			<ul>
-				<li class="nav-element"><a href="./rebecca.php">About Rebecca</a></li>
+				<li class="nav-element"><a href="./rebecca.php">About Rebecca Jones</a></li>
 				<li class="nav-element"><a href="./conceptual-model.php">Conceptual Model</a></li>
 				<li class="nav-element"><a href="./use-case.php">Use Case</a></li>
 			</ul>

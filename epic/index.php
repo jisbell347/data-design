40ff4815f1ea092579b6ba31f90f8c6170e7e7f6
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-11 20:04:22 -0600

    Added boilerplate HTML to document. Added nav bar list items

---
+++ epic/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Data Design Project</title>
+		<link rel="stylesheet" type="text/css" href="./css/styles.css"/>
+	</head>
+	<body>
+		<nav class="nav nav-bar">
+			<ul>
+				<li class="nav-element"><a href="rebecca.php">About Rebecca</a></li>
+				<li class="nav-element"><a href="conceptual-model.php">Conceptual Model</a></li>
+				<li class="nav-element"><a href="use-case.php">Use Case</a></li>
+			</ul>
+		</nav>
+	</body>
+</html>

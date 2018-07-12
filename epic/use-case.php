43222f6dcb59d87632b2088d1be68095f6e5b0e5
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-11 23:02:59 -0600

    Added boilerplate html and interaction flow chart

---
+++ epic/use-case.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Use Case</title>
+	</head>
+	<body>
+		<div>
+			<h1>Use Case</h1>
+			<p>Rebecca needs to find similar books</p>
+			<ul>
+				<li>Rebecca clicks on the browse tab located on the navigation bar.</li>
+				<li>Server causes the navigation to dropdown and reveal hidden list elements</li>
+				<li>Rebecca clicks on a genre she is interested in</li>
+				<li>Server displays a list of books related to her selection</li>
+				<li>Rebecca scrolls through the list of displayed books and clicks on a book she likes</li>
+				<li>Server opens the book details in a new page</li>
+				<li>Rebecca clicks on the "Want to read button"</li>
+				<li>Server displays a list of options for her to choose</li>
+				<li>Rebecca clicks on the choice: "Add to shelf"</li>
+				<li>Server adds the book to Rebecca's list of books</li>
+			</ul>
+		</div>
+		<div>
+			<button class="btn">
+				<a href="./index.php">Back to Home</a>
+			</button>
+		</div>
+	</body>
+</html>

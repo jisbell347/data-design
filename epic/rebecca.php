768d3ea655a634dd926a966fc138c94f7cae0146
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-11 20:38:51 -0600

    Added boilerplate HTML, image of rebecca and several paragraphs.

---
+++ epic/rebecca.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Persona of Rebecca</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css"/>
+	</head>
+	<body>
+		<h1>Rebecca Jones</h1>
+		<div class="pic">
+			<img src="rebecca.jpg" alt="Picture of Rebecca Jones" width="200px" height="auto"/>
+		</div>
+		<h2>Persona</h2>
+			<p><span class="bold">Name:</span>Rebecca Jones</p>
+			<p><span class="bold">Gender:</span>Female</p>
+			<p><span class="bold">Age:</span>47</p>
+			<p><span class="bold">Marital Status:</span>Married w/kids</p>
+			<p><span class="bold">Profession:</span>Stay at Home Mom</p>
+	</body>
+</html>

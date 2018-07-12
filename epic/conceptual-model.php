f8d0c87b02705349f3cd0af9895d1070d0b3a4bd
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-12 13:45:40 -0600

    Removed profiles and rates. Added bookgenres entity

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -10,36 +10,27 @@
 			<h1>Conceptual Model</h1>
 			<hr />
 			<h3>Entities & Attributes</h3>
-			<h4>Profile</h4>
-			<ul>
-				<li>profileId (Primary Key)</li>
-				<li>profileEmail</li>
-				<li>profileName</li>
-				<li>profileHash (password authentication)</li>
-			</ul>
 			<h4>Genre</h4>
 			<ul>
 				<li>genreId (Primary Key)</li>
-				<li>genreProfileId (Foreign Key)</li>
 				<li>genreType</li>
 			</ul>
 			<h4>Book</h4>
 			<ul>
 				<li>bookId (Primary Key)</li>
 				<li>bookGenreId (Foreign Key)</li>
-				<li>bookProfileId (Foreign Key)</li>
 				<li>bookTitle</li>
 				<li>bookAuthor</li>
+				<li>bookDescription</li>
 				<li>bookPublishDate</li>
 				<li>bookPages</li>
 			</ul>
-			<h4>Rates</h4>
+			<h4>Bookgenres</h4>
 			<ul>
-				<li>ratesBookId (Foreign Key)</li>
-				<li>ratesProfileId (Foreign Key)</li>
-				<li>ratesNumber</li>
-				<li>ratesDetails</li>
-				<li>ratesAverage</li>
+				<li>bookgenresId (Primary Key)</li>
+				<li>bookgenreBookId (Foreign Key)</li>
+				<li>bookgenresGenreId (Foreign Key)</li>
+				<li>bookgenreUsers</li>
 			</ul>
 		</div>
 		<div>
@@ -47,8 +38,6 @@
 			<ul>
 				<li>One genre contains many books (1-to-n)</li>
 				<li>Many books belong to multiple genres (m-to-n)</li>
-				<li>One user is able to rate many books (1-to-n)</li>
-				<li>Many users are able to rate multiple books(m-to-n)</li>
 			</ul>
 		</div>
 		<div>

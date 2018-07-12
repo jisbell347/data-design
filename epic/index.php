b3c4a3534a7df0bba612614cafc416439d3eb8e1
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-11 22:30:12 -0600

    Added header and main elements. Added img

--- epic/index.php
+++ epic/index.php
@@ -6,12 +6,19 @@
 		<link rel="stylesheet" type="text/css" href="./css/styles.css"/>
 	</head>
 	<body>
-		<nav class="nav nav-bar">
-			<ul>
-				<li class="nav-element"><a href="./rebecca.php">About Rebecca Jones</a></li>
-				<li class="nav-element"><a href="./conceptual-model.php">Conceptual Model</a></li>
-				<li class="nav-element"><a href="./use-case.php">Use Case</a></li>
-			</ul>
-		</nav>
+		<header>
+			<nav class="nav nav-bar">
+				<ul>
+					<li class="nav-element"><a href="./rebecca.php">About Rebecca Jones</a></li>
+					<li class="nav-element"><a href="./conceptual-model.php">Conceptual Model</a></li>
+					<li class="nav-element"><a href="./use-case.php">Use Case</a></li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+			<div class="meme">
+				<img src="https://i.pinimg.com/originals/10/07/4e/10074ed095e0a9324c6172d06efc6a51.jpg">
+			</div>
+		</main>
 	</body>
 </html>

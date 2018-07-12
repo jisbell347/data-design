3eec6051fb707b8ba92be1312b19b1a00848d125
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-11 20:59:54 -0600

    Added attitude of persona and a list of her needs

--- epic/rebecca.php
+++ epic/rebecca.php
@@ -10,11 +10,25 @@
 		<div class="pic">
 			<img src="rebecca.jpg" alt="Picture of Rebecca Jones" width="200px" height="auto"/>
 		</div>
-		<h2>Persona</h2>
-			<p><span class="bold">Name:</span>Rebecca Jones</p>
-			<p><span class="bold">Gender:</span>Female</p>
-			<p><span class="bold">Age:</span>47</p>
-			<p><span class="bold">Marital Status:</span>Married w/kids</p>
-			<p><span class="bold">Profession:</span>Stay at Home Mom</p>
+		<div>
+			<h2>Persona</h2>
+				<p><span class="bold">Name:</span>Rebecca Jones</p>
+				<p><span class="bold">Gender:</span>Female</p>
+				<p><span class="bold">Age:</span>47</p>
+				<p><span class="bold">Marital Status:</span>Married w/kids</p>
+				<p><span class="bold">Profession:</span>Stay at Home Mom</p>
+		</div>
+		<div>
+			<h2>Attitude</h2>
+			<p>Rebecca is a stay-at-home mom who enjoys reading on her downtime. She loves to read but often does not know which books to consider when choosing a book. She is open to suggestions and recieving help in choosing.</p>
+		</div>
+		<div>
+			<h2>Needs</h2>
+			<ul>
+				<li>Wants to be able to select books by genre</li>
+				<li>Wants to be able to see books similar to her intrests</li>
+				<li>Wants to be able to read and leave comments on books</li>
+			</ul>
+		</div>
 	</body>
 </html>

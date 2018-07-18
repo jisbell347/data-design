8084b715ab390d6cc187052a1ad0498f8de5e5ba
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-18 11:26:48 -0600

    Fixed typo in initial doc block

--- php/classes/Book.php
+++ php/classes/Book.php
@@ -3,7 +3,7 @@
  * Small Cross Section of the Goodreads site book section within a genre.
  *
  * This is a small example of what sites like Goodreads displays when a user clicks on a genre of choice and a list of
- books are displayed within that genre. The user then clicks on a specific book which brings them to that book's page.
+ * books are displayed within that genre. The user then clicks on a specific book which brings them to that book's page.
  * This can easily be extended to emulate more features of Goodreads.
  *
  * @author Joseph Isbell <jisbell1@cnm.edu>

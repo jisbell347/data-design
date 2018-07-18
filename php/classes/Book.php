d0b2821d247594b2d1bab34277c02f2e6dd223c8
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-18 09:37:41 -0600

    Created Book.php file. Added initial doc block and declared Book class

---
+++ php/classes/Book.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Small Cross Section of the Goodreads site book section within a genre.
+ *
+ * This is a small example of what sites like Goodreads displays when a user clicks on a genre of choice and a list of
+ books are displayed within that genre.
+ * This can easily be extended to emulate more features of Goodreads.
+ *
+ * @author Joseph Isbell <jisbell1@cnm.edu>
+ * @version 1.0.0
+ **/
+
+class Book {
+
+	/**
+	 *
+	 */
+}

f134b4629bc8bb7faece892a0f7a5c69eb3f5a37
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-19 14:03:50 -0600

    Added require autoloaders. Added sql connections

--- php/classes/Genre.php
+++ php/classes/Genre.php
@@ -1,5 +1,9 @@
 <?php
 namespace Edu\Cnm\DataDesign;
+
+require_once ('autoload.php');
+require_once (dirname(__DIR__, 2) . "/vendor/autoload.php");
+
 use Ramsey\Uuid\Uuid;
 /**
  * Small Cross Section of the Goodreads site genre menu.
@@ -110,4 +114,149 @@ class Genre {
 		//Store value once it passes validation
 		$this->genreType = $newGenreType;
 	}
+
+	/**
+	 * Inserts this Genre into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $pdo) : void {
+		//create query template
+		$query = "INSERT INTO genre(genreId, genreType) VALUES( :genreId, :genreType)";
+		$statement = $pdo->prepare($query);
+		//bind the member variables to the place holders in the template
+		$parameters = ["genreId"=>$this->genreId->getBytes(), "genreType"=>$this->genreType];
+		$statement->execute($parameters);
+	}
+
+	/**
+	 * deletes this genre from mySQL
+	 *
+	 * @param \PDO $pdo connection object
+	 * @throws \PDOException when mySQL errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function delete(\PDO $pdo) : void {
+		//creates the query template
+		$query = "DELETE FROM genre WHERE genreId = :genreId";
+		$statement = $pdo->prepare($query);
+
+		//bind the member variable to the place holder in the template
+		$parameters = ["genreId" => $this->genreId->getBytes()];
+		$statement->execute($parameters);
+	}
+
+	/**
+	 * Updates the genre in mySQL
+	 *
+	 * @param \PDO $pdo connection object
+	 * @throws \PDOException when mySQL errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 */
+	public function update(\PDO $pdo) : void {
+		//create query template
+		$query = "UPDATE genre SET genreType = :genreType WHERE genreId = :genreId";
+		$statement = $pdo->prepare($query);
+
+		//binds the members to the template
+		$parameters = ["genreId" => $this->genreId->getBytes(), "genreType" => $this->genreType];
+		$statement->execute($parameters);
+	}
+
+	/**
+	 * Gets the genre by genre id
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param Uuid|string $genreId genre id to search for
+	 * @return Genre|null genre found or not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when a variable are not the correct data type
+	 **/
+	public static function getGenreByGenreId(\PDO $pdo, $genreId) : ?Genre {
+		//sanitize the genreId before searching
+		try {
+			$genreId = self::validateUuid($genreId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw (new \PDOException($exception->getMessage(), 0, $exception));
+		}
+
+		//create the query template
+		$query = "SELECT genreId, genreType FROM genre WHERE genreId = :genreId";
+		$statement= $pdo->prepare($query);
+
+		// bind the genre id to the place holder in the template
+		$parameters = ["genreId" => $genreId->getBytes()];
+		$statement->execute($parameters);
+
+		//grab the genre from mySQL
+		try {
+			$genre = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$genre = new Genre($row["genreId"], $row["genreType"]);
+			}
+		} catch(\Exception $exception) {
+			//if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return($genre);
+	}
+	/**
+	 * Gets the genre by type
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param string $genreType genre type to search for
+	 * @return \SplFixedArray SplFixedArray of genres found
+	 * @throws \PDOException when mySQL errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 */
+	public static function getGenreByGenreType(\POO $pdo, string $genreType) : \SplFixedArray {
+		//sanitize the type before searching
+		$genreType = trim($genreType);
+		$genreType = filter_var($genreType, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($genreType) === true) {
+			throw(new \PDOException("Genre type cannot be empty"));
+		}
+
+		//escape any mySQL wild cards
+		$genreType = str_replace("_", "\\_", str_replace("&", "\\&", $genreType));
+
+		//create query template
+		$query = "SELECT genreId, genreType FROM genre WHERE genreType LIKE :genreType";
+		$statement = $pdo->prepare($query);
+
+		// bind the members to the template place holder
+		$genreType = "%$genreType%";
+		$parameters = ["genreType" => $genreType];
+		$statement->execute($parameters);
+
+		//build an array of genres
+		$genres = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$genre = new Genre($row["genreId"], $row["genreType"]);
+				$genres[$genres->key()] = $genre;
+				$genres->next();
+			} catch(\Exception $exception) {
+				//if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return($genres);
+	}
+	/** Gets all genres
+	*
+	* @param \PDO $pdo PDO connection object
+	* @return \SplFixedArray SplFixedArray of genres found
+	* @throws \PDOException when mySQL errors occur
+	* @throws \TypeError when variables are not the correct data type
+	**/
+	public static function getAllTweets(\PDO $pdo) : \SplFixedArray {
+
+	}
+
 }

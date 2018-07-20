d0cf727b78a261a5c353b4f4f9a893cbc68ccddc
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-19 21:49:19 -0600

    Added code to insert, update, delete and select from book table

--- sql/oml.sql
+++ sql/oml.sql
@@ -9,4 +9,11 @@ UPDATE genre SET genreType = "Sci-Fi" WHERE genreId = UNHEX("8c38a40ddec34a469b8
 
 DELETE FROM genre WHERE genreId = UNHEX("8c38a40ddec34a469b8708f059ae6df6");
 
- INSERT INTO book(bookId, bookGenreId, bookAuthor, bookDescription, bookPages, bookPublishDate, bookTitle) VALUES (UNHEX("c6b33f751c9e413a90b8ae9f077756e3"), unhex("0225722944b54d56be0929d0fd82dc03"), "Shawn Spencer", "Shawn details his amazing gift in this best selling book", 582, 2015, "Pysch");
+INSERT INTO book(bookId, bookGenreId, bookAuthor, bookDescription, bookPages, bookPublishDate, bookTitle) VALUES (UNHEX("c6b33f751c9e413a90b8ae9f077756e3"), unhex("0225722944b54d56be0929d0fd82dc03"), "Shawn Spencer", "Shawn details his amazing gift in this best selling book", 582, 2015, "Pysch");
+
+ UPDATE book SET bookPublishDate = 2018 WHERE bookId = UNHEX("c6b33f751c9e413a90b8ae9f077756e3");
+
+ SELECT *
+ FROM book;
+
+ DELETE FROM book WHERE bookId = UNHEX("c6b33f751c9e413a90b8ae9f077756e3");

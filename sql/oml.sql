ed6e59622533c344bb3e5d03433e13eb9c3667b0
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-19 22:54:36 -0600

    Rearranged code for better flow

--- sql/oml.sql
+++ sql/oml.sql
@@ -1,28 +1,28 @@
  -- INSERT Statements
 
-INSERT INTO genre(genreId, genreType) VALUES (UNHEX("0225722944b54d56be0929d0fd82dc03"), "Romance");
+INSERT INTO genre(genreId, genreType) VALUES (UNHEX("ea579028c78144c9a4ff13d8967f929d"), "Manga");
 
-UPDATE genre SET genreType = "Sci-Fi" WHERE genreId = UNHEX("8c38a40ddec34a469b8708f059ae6df6");
+UPDATE genre SET genreType = "Comic" WHERE genreId = UNHEX("ea579028c78144c9a4ff13d8967f929d");
+
+INSERT INTO book(bookId, bookGenreId, bookAuthor, bookDescription, bookPages, bookPublishDate, bookTitle) VALUES (UNHEX("c6b33f751c9e413a90b8ae9f077756e3"), unhex("ea579028c78144c9a4ff13d8967f929d"), "Shawn Spencer", "Shawn details his amazing gift in this best selling book", 582, 2015, "Pysch");
+
+UPDATE book SET bookPublishDate = 2018 WHERE bookId = UNHEX("c6b33f751c9e413a90b8ae9f077756e3");
+
+INSERT INTO bookGenre(bookgenreId, bookgenreBookId, bookgenreGenreId) VALUES (UNHEX("ed7e623dea144cc09cd8152f39daa948"), unhex("ea579028c78144c9a4ff13d8967f929d"), UNHEX("c6b33f751c9e413a90b8ae9f077756e3"));
+
+UPDATE bookGenre SET bookgenreGenreId = UNHEX("8c38a40ddec34a469b8708f059ae6df6");
 
  SELECT *
- FROM genre;
+ FROM genre, book, bookGenre;
 
-DELETE FROM genre WHERE genreId = UNHEX("8c38a40ddec34a469b8708f059ae6df6");
-
-INSERT INTO book(bookId, bookGenreId, bookAuthor, bookDescription, bookPages, bookPublishDate, bookTitle) VALUES (UNHEX("c6b33f751c9e413a90b8ae9f077756e3"), unhex("0225722944b54d56be0929d0fd82dc03"), "Shawn Spencer", "Shawn details his amazing gift in this best selling book", 582, 2015, "Pysch");
-
- UPDATE book SET bookPublishDate = 2018 WHERE bookId = UNHEX("c6b33f751c9e413a90b8ae9f077756e3");
-
- SELECT *
- FROM book;
+ DELETE FROM bookGenre WHERE bookgenreId = UNHEX("ed7e623dea144cc09cd8152f39daa948");
 
  DELETE FROM book WHERE bookId = UNHEX("c6b33f751c9e413a90b8ae9f077756e3");
 
- INSERT INTO bookGenre(bookgenreId, bookgenreBookId, bookgenreGenreId) VALUES (UNHEX("ed7e623dea144cc09cd8152f39daa948"), unhex("0225722944b54d56be0929d0fd82dc03"), UNHEX("c6b33f751c9e413a90b8ae9f077756e3"));
+ DELETE FROM genre WHERE genreId = UNHEX("8c38a40ddec34a469b8708f059ae6df6");
 
- UPDATE bookGenre SET bookgenreGenreId = UNHEX("8c38a40ddec34a469b8708f059ae6df6");
 
-SELECT *
- FROM bookGenre;
 
-DELETE FROM bookGenre WHERE bookgenreId = UNHEX("ed7e623dea144cc09cd8152f39daa948");
+
+
+

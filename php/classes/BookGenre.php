4ca8afab9b658a183cca2724ecb863464df88551
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-19 22:41:11 -0600

    Added inserts, updates, deletes and selects methods

--- php/classes/BookGenre.php
+++ php/classes/BookGenre.php
@@ -138,5 +138,203 @@ class BookGenre {
 		//Converts and stores the book genre genre id
 		$this->bookGenreGenreId = $uuid;
 	}
+	/**
+	 * inserts this BookGenre into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $pdo) : void {
+		//create query template
+		$query = "INSERT INTO bookGenre(bookgenreId, bookgenreBookId, bookgenreGenreId) VALUES(:bookgenreId, :bookgenreBookId, :bookgenreGenreId)";
+		$statement = $pdo->prepare($query);
 
+		//bind the member variables to the place holders in the template
+		$parameters = ["bookGenreId" => $this->bookGenreId->getBytes(), "bookGenreBookId" => $this->bookGenreBookId->getBytes(), "bookGenreGenreId" => $this->bookGenreGenreId->getBytes()];
+		$statement->execute($parameters);
+	}
+
+	/**
+	 * deletes the BookGenre from mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function delete(\PDO $pdo) : void {
+		// create query template
+		$query = "DELETE FROM bookGenre WHERE bookgenreId = :bookgenreId";
+		$statement = $pdo->prepare($query);
+
+		//bind the member variables to the place holder in the template
+		$parameters = ["bookGenreId" => $this->bookGenreId->getBytes()];
+		$statement->execute($parameters);
+	}
+
+	/**
+	 * updates this BookGenre in mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function update(\PDO $pdo) : void {
+		// create query template
+		$query = "UPDATE bookGenre SET bookgenreBookId = :bookgenreBookId, bookgenreGenreId = :bookgenreGenreId WHERE bookgenreId = :bookgenreId";
+		$statement = $pdo->prepare($query);
+
+		//binds variables to the place holders in the template
+		$parameters = ["bookGenreId" => $this->bookGenreId->getBytes(), "bookGenreBookId" => $this->bookGenreBookId->getBytes(), "bookGenreGenreId" => $this->bookGenreGenreId->getBytes()];
+		$statement->execute($parameters);
+	}
+
+	/**
+	 * gets the BookGenre by bookGenreId
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param Uuid|string $bookGenreId book genre id to search for
+	 * @return BookGenre|null BookGenre found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public static function getBookGenreByBookGenreId(\PDO $pdo, $bookGenreId) : ?BookGenre {
+		//sanitize the string before searching
+		try {
+			$bookGenreId = self::validateUuid($bookGenreId);
+		} catch(\InvalidArgumentException | \RangeException |\Exception |\TypeError $exception) {
+			throw (new \PDOException($exception->getMessage(), 0, $exception));
+		}
+
+		//create query template
+		$query = "SELECT bookGenreId, bookGenreBookId, bookGenreGenreId FROM bookGenre WHERE bookgenreId = : bookgenreId";
+		$statement = $pdo->prepare($query);
+
+		//bind the bookgenre id to the place holder in the template
+		$parameters = ["bookGenreId" => $bookGenreId->getBytes()];
+		$statement->execute($parameters);
+
+		// grab the bookGenre from mySQL
+		try {
+			$bookGenre = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$bookGenre = new BookGenre($row["bookGenreId"], $row["bookGenreBookId"], $row["bookGenreGenreId"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw (new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return($bookGenre);
+	}
+	/**
+	 * gets the BookGenre by bookGenreBookId
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param Uuid|string $bookGenreBookId book genre book id to search for
+	 * @return \SplFixedArray SPLFixedArray of books in book genre found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public static function getBookGenreByBookGenreBookId(\PDO $pdo, $bookGenreBookId) : \SplFixedArray {
+		//sanitize the string before searching
+		try {
+			$bookGenreBookId = self::validateUuid($bookGenreBookId);
+		} catch(\InvalidArgumentException | \RangeException |\Exception |\TypeError $exception) {
+			throw (new \PDOException($exception->getMessage(), 0, $exception));
+		}
+
+		//create query template
+		$query = "SELECT bookGenreId, bookGenreBookId, bookGenreGenreId FROM bookGenre WHERE bookgenreBookId = : bookgenreBookId";
+		$statement = $pdo->prepare($query);
+
+		//bind the bookgenre id to the place holder in the template
+		$parameters = ["bookGenreBookId" => $bookGenreBookId->getBytes()];
+		$statement->execute($parameters);
+
+		//build array of books
+		$bookGenres = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$bookGenre = new BookGenre($row["bookGenreId"], $row["bookGenreBookId"], $row["bookGenreGenreId"]);
+				$bookGenres[$bookGenres->key()] = $bookGenre;
+				$bookGenres->next();
+			} catch(\Exception $exception) {
+				//if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return($bookGenres);
+	}
+	/**
+	 * gets the BookGenre by bookGenreGenreId
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param Uuid|string $bookGenreGenreId book genre genre id to search for
+	 * @return \SplFixedArray SPLFixedArray of books in book genre found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public static function getBookGenreByBookGenreGenreId(\PDO $pdo, $bookGenreGenreId) : \SplFixedArray {
+		//sanitize the string before searching
+		try {
+			$bookGenreGenreId = self::validateUuid($bookGenreGenreId);
+		} catch(\InvalidArgumentException | \RangeException |\Exception |\TypeError $exception) {
+			throw (new \PDOException($exception->getMessage(), 0, $exception));
+		}
+
+		//create query template
+		$query = "SELECT bookGenreId, bookGenreBookId, bookGenreGenreId FROM bookGenre WHERE bookgenreGenreId = :bookgenreGenreId";
+		$statement = $pdo->prepare($query);
+
+		//bind the bookgenre id to the place holder in the template
+		$parameters = ["bookGenreGenreId" => $bookGenreGenreId->getBytes()];
+		$statement->execute($parameters);
+
+		//build array of genres
+		$bookGenres = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$bookGenre = new BookGenre($row["bookGenreId"], $row["bookGenreBookId"], $row["bookGenreGenreId"]);
+				$bookGenres[$bookGenres->key()] = $bookGenre;
+				$bookGenres->next();
+			} catch(\Exception $exception) {
+				//if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return($bookGenres);
+	}
+	/**
+	 * gets all book genres
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @return \SplFixedArray SPLFixedArray of books in book genre found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public static function getAllBookGenres(\PDO $pdo) : \SplFixedArray {
+		//create query template
+		$query = "SELECT bookGenreId, bookGenreBookId, bookGenreGenreId FROM bookGenre";
+		$statement = $pdo->prepare($query);
+		$statement->execute();
+
+		//build array of genres
+		$bookGenres = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$bookGenre = new BookGenre($row["bookGenreId"], $row["bookGenreBookId"], $row["bookGenreGenreId"]);
+				$bookGenres[$bookGenres->key()] = $bookGenre;
+				$bookGenres->next();
+			} catch(\Exception $exception) {
+				//if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return($bookGenres);
+	}
 }

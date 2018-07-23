c8c0922a0ffeb76221df47a8c4ebff9f748b3ce6
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-23 11:20:57 -0600

    fixed indentation

--- php/classes/autoload.php
+++ php/classes/autoload.php
@@ -10,31 +10,31 @@
 * @see http://www.php-fig.org/psr/psr-4/examples/ PSR-4 Example Autoloader
 **/
 spl_autoload_register(function($class) {
-/**
-* CONFIGURABLE PARAMETERS
-* prefix: the prefix for all the classes (i.e., the namespace)
-* baseDir: the base directory for all classes (default = current directory)
-**/
-$prefix = "Jisbell347\\DataDesign";
-$baseDir = __DIR__;
+	/**
+	* CONFIGURABLE PARAMETERS
+	* prefix: the prefix for all the classes (i.e., the namespace)
+	* baseDir: the base directory for all classes (default = current directory)
+	**/
+	$prefix = "Jisbell347\\DataDesign";
+	$baseDir = __DIR__;
 
-// does the class use the namespace prefix?
-$len = strlen($prefix);
-if (strncmp($prefix, $class, $len) !== 0) {
-// no, move to the next registered autoloader
-return;
-}
+	// does the class use the namespace prefix?
+	$len = strlen($prefix);
+	if (strncmp($prefix, $class, $len) !== 0) {
+	// no, move to the next registered autoloader
+	return;
+	}
 
-// get the relative class name
-$className = substr($class, $len);
+	// get the relative class name
+	$className = substr($class, $len);
 
-// replace the namespace prefix with the base directory, replace namespace
-// separators with directory separators in the relative class name, append
-// with .php
-$file = $baseDir . str_replace("\\", "/", $className) . ".php";
+	// replace the namespace prefix with the base directory, replace namespace
+	// separators with directory separators in the relative class name, append
+	// with .php
+	$file = $baseDir . str_replace("\\", "/", $className) . ".php";
 
-// if the file exists, require it
-if(file_exists($file)) {
-require_once($file);
-}
+	// if the file exists, require it
+	if(file_exists($file)) {
+	require_once($file);
+	}
 });
